148d1edc8545cbaf29097ba11a97b3c2f9723bdb
redsonlopez <redsonlopez@gmail.com>
2026-03-17 22:10:49 -0300

    change api/schemas.py examples

--- src/prediction_diabetes/api/schemas.py
+++ src/prediction_diabetes/api/schemas.py
@@ -4,9 +4,9 @@ from prediction_diabetes.config import FEATURE_COLUMNS
 
 
 class DiabetesInput(BaseModel):
-    age: int = Field(..., example=48)
-    physical_activity_minutes_per_week: int = Field(..., example=143)
-    diet_score: float = Field(..., example=6.7)
-    screen_time_hours_per_day: float = Field(..., example=8.7)
-    bmi: float = Field(..., example=23.1)
+    age: int = Field(..., example=23)
+    physical_activity_minutes_per_week: int = Field(..., example=360)
+    diet_score: float = Field(..., example=9.0)
+    screen_time_hours_per_day: float = Field(..., example=1.0)
+    bmi: float = Field(..., example=21.5)
 
